d18515b43a60078c41ca0cc5f3d66446f44fac44
Drakula44 <ninadrakulic04@gmail.com>
2022-08-06 12:28:56 +0200

    moze da se nadje graf kola, i izvuceni su bounding boxovi detalja koja treba dodati svakom elementu

--- src/flood_fill_tools.py
+++ src/flood_fill_tools.py
@@ -75,7 +75,7 @@ def find_closest(matrix, target, radius):
     nonzero = np.zeros(matrix.shape)
     nonzero[a] = 1
 
-def extract_largest_component(img):
+def split_circuit(img):
     width = len(img)
     height = len(img[0])
     original = img.copy()
@@ -89,7 +89,7 @@ def extract_largest_component(img):
                 img = filled
     elements.sort(reverse=True)
     without = flood_fill(original, elements[0][1], 0)
-    return original - without
+    return original - without, without
 
 def find_remeaning_components(img):
     width = len(img)
@@ -112,6 +112,35 @@ def find_remeaning_components(img):
         aspect_ratio = (ystop - ystart) / (xstop - xstart)
         if (aspect_ratio < 0.5 or aspect_ratio > 2) and e[0] > 100:
             center = (int((xstop + xstart) / 2), int((ystop + ystart) / 2))
-            resistors.append([center, (ystart, xstart), (ystop, xstop)])
+            resistors.append([center, (xstart, ystart), (xstop, ystop)])
             cv2.rectangle(original, (xstart, ystart), (xstop, ystop), 125, -1)
     return resistors
+
+
+def get_centers(img):
+    centers = []
+    for i in range(len(img)):
+        for j in range(len(img[0])):
+            if img[i][j] != 0:
+                flood_img = flood(img, (i,j))
+                mass_x, mass_y = np.where(flood_img > 0)
+                # mass_x and mass_y are the list of x indices and y indices of mass pixels
+                cent_y = int(np.average(mass_x))
+                cent_x = int(np.average(mass_y))
+                centers.append((cent_x, cent_y))
+                flood_fill(img, (i,j), 0, in_place=True)
+    return centers
+
+def find_bounding_box_details(img):
+    width = len(img)
+    height = len(img[0])
+    details = []
+    for i in range(width):
+        for j in range(height):
+            if img[i][j] == 0:
+                continue
+            B = np.argwhere(flood(img, (i,j)))
+            (ystart, xstart), (ystop, xstop) = B.min(0), B.max(0) + 1
+            details.append([np.array([xstart, ystart]), np.array([xstop, ystop])])
+            flood_fill(img, (i,j), 0, in_place=True)
+    return details
--- src/tools.py
+++ src/tools.py
@@ -2,7 +2,9 @@ import numpy as np
 from skimage.filters import sobel_h, sobel_v
 from skimage.segmentation import flood, flood_fill
 import matplotlib.pyplot as plt
-from flood_fill_tools import find_remeaning_components
+from flood_fill_tools import find_remeaning_components,get_centers
+from skimage.morphology import skeletonize
+
 import cv2
 
 def detect_circles(img):
@@ -54,10 +56,110 @@ def get_resistors_cords(img, circuit_graph_img):
 
 def visualize_components(rgb_img, gen_cords, resistor_cords):
     for e in resistor_cords:
-        e = e[0]
-        cv2.circle(rgb_img, (e[0], e[1]), 10, (255, 0, 255), -1)
+        cv2.circle(rgb_img, e[0], 1, (255, 0, 255), -1)
+        cv2.rectangle(rgb_img, e[1], e[2], (255, 0, 255), 2)
 
     for e in gen_cords:
-        cv2.circle(rgb_img, (e[0], e[1]), 10, (0, 0, 255), -1)
-    
+        cv2.circle(rgb_img, (e[0], e[1]), int(e[2]*1.1), (0, 0, 255), 2)
+        cv2.circle(rgb_img, (e[0], e[1]), 1, (0, 0, 255), -1)
+
     return rgb_img
+
+def find_connections(img, start_point, end_point):
+    # scale img to 0-1
+    img = img.copy()
+    img = img / 255
+
+    img = skeletonize(img)
+    img = img.astype(np.uint8)*255
+    starty, startx  = start_point
+    endy, endx = end_point
+    offset = 2
+    startx -= offset
+    starty -= offset
+    endx += offset
+    endy += offset
+    ends = []
+    for i in range(startx, endx):
+        if img[i, starty] == 255:
+            ends.append((i, starty))
+        if img[i, endy] == 255:
+            ends.append((i, endy))
+    for j in range(starty, endy):
+        if img[startx, j] == 255:
+            ends.append((startx, j))
+        if img[endx, j] == 255:
+            ends.append((endx, j))
+    if len(ends) == 0:
+        print("aaaaa")
+        return
+    if len(ends) != 2:
+        print("Error: more than 2 ends found")
+        return
+    cv2.line(img, (ends[0][1], ends[0][0]), (ends[1][1], ends[1][0]), (255,255,255), 1)
+    return img
+
+def turn_off_components(img, resistor_cords, gen_cords):
+    for e in resistor_cords:
+        cv2.rectangle(img, e[1], e[2], (0,0,0), -1)
+        img = find_connections(img, e[1], e[2])
+    for e in gen_cords:
+        r = int(e[2]*1.22)
+        cv2.circle(img, (e[0], e[1]), r, (0,0,0), -1)
+        img = find_connections(img, (e[0]-r, e[1]-r), (e[0]+r, e[1]+r))
+    return img
+
+def find_lines(img):
+    img = img.copy()
+    # find lines in image with HoughLines
+    # lines = cv2.HoughLines(img, 1, np.pi / 180, 100, None, 0, 0)
+    lines = cv2.HoughLinesP(img, 1, np.pi / 180, 50, None, 50, 10)
+    if lines is None:
+        print("No lines found")
+        return img
+    # convert lines to numpy array
+    return lines
+
+def visualize_lines(img, lines):
+    # for i in range(0, len(lines)):
+    #     rho = lines[i][0][0]
+    #     theta = lines[i][0][1]
+    #     a = np.cos(theta)
+    #     b = np.sin(theta)
+    #     x0 = a * rho
+    #     y0 = b * rho
+    #     pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+    #     pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+    #     cv2.line(img, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
+    for i in range(0, len(lines)):
+        l = lines[i][0]
+        cv2.line(img, (l[0], l[1]), (l[2], l[3]), (0,255,0), 1)
+    return img
+
+
+def extract_junctions(img):
+    kernel = np.ones((3, 3), np.uint8)
+    img = cv2.erode(img, kernel, iterations=2)
+    img = cv2.dilate(img, kernel, iterations=2)
+    centers = get_centers(img)
+    return centers
+
+def visualize_junctions(img, centers):
+    for e in centers:
+        cv2.circle(img, e, 1, (255, 0, 0), -1)
+    return img
+
+
+def cluster_symbols(img):
+    # dilate img to get bigger symbols
+    kernel = np.ones((5, 5), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=2)
+    img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    return img
+
+def assign_details(details, gen_cords, resistor_cords, nodes):
+    for r in resistor_cords:
+        for n in nodes:
+            if n[0] == r[0] and n[1] == r[1]:
+                details.append((n, r))
+                break
